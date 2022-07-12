fce91a1fd04cb148d7c90d5df24b1d7d52d099d9
DaniilKorzun <korzundanik@gmail.com>
2022-07-12 11:53:22 +0300

    Create project

---
+++ src/main/java/by/korzun/cryptocurrencywatcher/CryptocurrencyWatcherApplication.java
@@ -0,0 +1,13 @@
+package by.korzun.cryptocurrencywatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocurrencyWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/korzun/cryptocurrencywatcher/CryptocurrencyWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package by.korzun.cryptocurrencywatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
