ae8abe2b402cf21f42a48ff10ed2ea3f8f4e48d6
DaniilKorzun <korzundanik@gmail.com>
2022-07-13 23:35:54 +0300

    Create ExceptionResolver for handling errors

---
+++ src/main/java/by/korzun/cryptocurrencywatcher/exception/ExceptionResolver.java
@@ -0,0 +1,16 @@
+package by.korzun.cryptocurrencywatcher.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ExceptionResolver {
+    @ExceptionHandler(CryptocurrencyNotFound.class)
+    public ResponseEntity<Object> handleCryptocurrencyNotFoundException(CryptocurrencyNotFound e) {
+        return new ResponseEntity<>(new Date() + " " + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
