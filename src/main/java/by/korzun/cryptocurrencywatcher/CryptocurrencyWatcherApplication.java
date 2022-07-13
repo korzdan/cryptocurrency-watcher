4619db1763d38c9574e4138584a7ebd0d4e45d3f
DaniilKorzun <korzundanik@gmail.com>
2022-07-13 23:34:00 +0300

    Enable scheduling in application

--- src/main/java/by/korzun/cryptocurrencywatcher/CryptocurrencyWatcherApplication.java
+++ src/main/java/by/korzun/cryptocurrencywatcher/CryptocurrencyWatcherApplication.java
@@ -2,8 +2,10 @@ package by.korzun.cryptocurrencywatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CryptocurrencyWatcherApplication {
 
     public static void main(String[] args) {
