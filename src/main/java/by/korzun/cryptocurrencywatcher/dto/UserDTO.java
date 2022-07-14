9be51cbb262872f24a5c80ba2f627a9b05802c6d
DaniilKorzun <korzundanik@gmail.com>
2022-07-14 03:31:41 +0300

    Create UserDTO for notify() method

---
+++ src/main/java/by/korzun/cryptocurrencywatcher/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package by.korzun.cryptocurrencywatcher.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String username;
+    private String symbol;
+}
